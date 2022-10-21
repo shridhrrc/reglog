4ae0180d6de02ac5f2ed80baa29813117b38eb97
shridhar karigoudar <39985652+shridhrrc@users.noreply.github.com>
2022-10-21 19:33:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
-mongoose.connect('mongodb://admin:admin@172.16.2.4/sectest', {
+mongoose.connect('mongodb://admin:admin@172.16.2.7/sectest', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err) => {
